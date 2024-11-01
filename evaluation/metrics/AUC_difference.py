443112600ec5d90f1e6c9fedce65aa1d76cabf02
Nikos <papadeas.nikos@gmail.com>
2024-11-01 15:05:37 +0000

    updated opposite class method, added std to AUC diff

--- evaluation/metrics/AUC_difference.py
+++ evaluation/metrics/AUC_difference.py
@@ -17,20 +17,27 @@ class AUIDC_metric():
         self.y_test = y_test
 
         self.classes = np.unique(y_train) #TODO: this is supposed to be label_mapping but label mapping currently doesn't have the class labels
-        self.opposite_class_dict = {}
+        self.mean_class_dict = dict.fromkeys(self.classes)
+        self.opposite_class_dict = dict.fromkeys(self.classes)
+
         for unique_class in self.classes:
-            opposite_class_idxs = ~(y_train==unique_class)
-            opposite_sample = np.mean(X_train[opposite_class_idxs], axis=0)
-            self.opposite_class_dict[unique_class] = {"sample": opposite_sample, "pred": None}
+            class_idxs = (y_train==unique_class)
+            mean_class_sample = np.mean(X_train[class_idxs], axis=0)
+            self.mean_class_dict[unique_class] = {"sample": mean_class_sample, "pred": None}
             # print(opposite_class_dict[unique_class]["pred"])
+
 
     def fit_ml_model(self, ml_model):
 
         for unique_class in self.classes:
-            opposite_sample = self.opposite_class_dict[unique_class]["sample"]
-            self.opposite_class_dict[unique_class]["pred"] = predict_proba(ml_model, opposite_sample[None, :])[0]
-            # except AttributeError:
-            #     opposite_class_dict[unique_class]["pred"] = np.array(ml_model.predict(opposite_sample[None, :])[0], ndmin=1)
+            mean_class_sample = self.mean_class_dict[unique_class]["sample"]
+            mean_class_pred = predict_proba(ml_model, mean_class_sample[None, :])[0]
+            self.mean_class_dict[unique_class]["pred"] = mean_class_pred
+
+        for target_class in self.classes:
+            mean_class_preds_for_target_class = {unique_class: self.mean_class_dict[i]["pred"][target_class] for unique_class in self.classes} # {class: prediction of avg_class_sample for target_class}
+            opposite_class = min(mean_class_preds_for_target_class, key=mean_class_preds_for_target_class.get)
+            self.opposite_class_dict[target_class] = opposite_class
 
 
     def normalize_saliency(self, saliency):
@@ -42,8 +49,8 @@ class AUIDC_metric():
         # A and B are flattened, and so is salient order
 
         preds = [A_pred]
-        perturbed_sample = copy.deepcopy(A)
-        replacement_sample = copy.deepcopy(B)
+        perturbed_sample = np.copy(A)
+        replacement_sample = np.copy(B)
         start_points = self.change_points[:-1]
         end_points = self.change_points[1:]
         for start_idx, end_idx in zip(start_points, end_points): # very last segment is not computed since it will result in B
@@ -78,33 +85,35 @@ class AUIDC_metric():
             sample_pred = y_test_pred[sample_idx] # TODO: this is the normal line of code, below a temporary until y_pred format is fixed
             # sample_pred = predict_proba(ml_model, sample[None, :])[0]
 
-            sample_pred_class_idx = np.argmax(sample_pred)
-            target_class_idx = sample_pred_class_idx
-            sample_pred_class = self.classes[target_class_idx]
-            sample_pred_target = sample_pred[target_class_idx]
-            opposite_sample = self.opposite_class_dict[sample_pred_class]["sample"]
-            opposite_pred_target_class = self.opposite_class_dict[sample_pred_class]["pred"][target_class_idx]
+            predicted_class_idx = np.argmax(sample_pred)
+            predicted_class = self.classes[predicted_class_idx]
+            prediction_for_class = sample_pred[predicted_class_idx]
+            opposite_class = self.opposite_class_dict[predicted_class]
+            opposite_sample = self.mean_class_dict[opposite_class]["sample"]
+            opposite_prediction_for_class = self.mean_class_dict[opposite_class]["pred"][predicted_class_idx]
 
             A = sample.reshape(self.n_features)
-            A_pred = sample_pred_target
+            A_pred = prediction_for_class
             B = opposite_sample.reshape(self.n_features)
-            B_pred = opposite_pred_target_class
-            AUC_deletion = self.calc_AUC_score(A, A_pred, B, B_pred, salient_order, target_class_idx, ml_model, mode="deletion")
+            B_pred = opposite_prediction_for_class
+            AUC_deletion = self.calc_AUC_score(A, A_pred, B, B_pred, salient_order, predicted_class_idx, ml_model, mode="deletion")
 
             A = opposite_sample.reshape(self.n_features)
-            A_pred = opposite_pred_target_class
+            A_pred = opposite_prediction_for_class
             B = sample.reshape(self.n_features)
-            B_pred = sample_pred_target
-            AUC_insertion = self.calc_AUC_score(A, A_pred, B, B_pred, salient_order, target_class_idx, ml_model, mode="insertion")
+            B_pred = prediction_for_class
+            AUC_insertion = self.calc_AUC_score(A, A_pred, B, B_pred, salient_order, predicted_class_idx, ml_model, mode="insertion")
 
             # AUC_diff = AUC_insertion - AUC_deletion
             AUC_insertion_array[sample_idx] = AUC_insertion
             AUC_deletion_array[sample_idx] = AUC_deletion
 
         mean_AUC_insertion = np.mean(AUC_insertion_array) # std
+        std_AUC_insertion = np.std(AUC_insertion_array)
         mean_AUC_deletion = np.mean(AUC_deletion_array) # std
+        std_AUC_deletion = np.std(AUC_deletion_array) # std
 
-        return (("insertion", mean_AUC_insertion), ("deletion", mean_AUC_deletion)) #, ("normalized", mean_normalized_AUC_diff)
+        return (("insertion", mean_AUC_insertion), ("deletion", mean_AUC_deletion), ("insertion", std_AUC_insertion), ("deletion", std_AUC_deletion)) #, ("normalized", mean_normalized_AUC_diff)
 
     def evaluate(self, attributions, y_test_pred, ml_model):
         return self.AUC_difference(attributions, y_test_pred, ml_model)
